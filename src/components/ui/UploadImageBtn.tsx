c8574ecb30fb2d9ec80470b32c7708b29e303ba5
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-01-09 19:45:39 +0200

    fix the UploadImageBtn component

--- src/components/ui/UploadImageBtn.tsx
+++ src/components/ui/UploadImageBtn.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useState, useRef, useEffect } from 'react';
-import { CloseOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
+import { useState, useRef } from 'react';
 import Image from 'next/image';
+import { CloseOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
 
 
 interface IUploadImageButton {
@@ -11,22 +11,26 @@ interface IUploadImageButton {
   required?: boolean;
   multiple?: boolean;
   defaultValue?: string;
-  value?: string | string[];
   error?: string[]
 }
 
 
-const UploadImageButton: React.FC<IUploadImageButton> = ({ name, label, required, multiple = false, defaultValue, value, error }) => {
+const UploadImageButton: React.FC<IUploadImageButton> = ({ 
+  name, 
+  label, 
+  required, 
+  multiple = false, 
+  defaultValue,
+  error 
+}) => {
   const [selectedFiles, setSelectedFiles] = useState<any>(defaultValue || [])
   const ref = useRef<HTMLInputElement>(null);
 
-  const convertFilesToString = (files: any[]) => {
+  const convertFileToString = (file: any) => {
     return new Promise((resolve, reject) => {
       const fileReader = new FileReader();
 
-      files.forEach((file: any) => {
-        fileReader.readAsDataURL(file);
-      });
+      fileReader.readAsDataURL(file);
 
       fileReader.onload = () => {
         resolve(fileReader.result);
@@ -43,18 +47,17 @@ const UploadImageButton: React.FC<IUploadImageButton> = ({ name, label, required
   };
 
   const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newFiles = await convertFilesToString(Array.from(e.target.files!));
-    setSelectedFiles([...selectedFiles, newFiles])
+    const data = Array.from(e.target.files!);
+    const pickedFiles = await Promise.all(data.map(async (item: any) => {
+      const url = await convertFileToString(item);
+      return url;
+    }));
+    setSelectedFiles([...selectedFiles, ...pickedFiles]);
   };
 
   const handleImageDelete = (value: any) => {
-    console.log('DELETE VALUE', value)
     setSelectedFiles((prevState: any[]) => prevState.filter(url => typeof url === 'string' ? url !== value : url.name !== value.name));
   };
-
-  useEffect(() => {
-    console.log('SELECTED FILES', selectedFiles)
-  }, [selectedFiles])
 
   return (
     <div className='w-full flex items-center gap-3'>
@@ -69,73 +72,53 @@ const UploadImageButton: React.FC<IUploadImageButton> = ({ name, label, required
           >
             Upload
           </button>
-          {value && selectedFiles.length === 0 ? (
-            <>
-              {Array.isArray(value) ? (
-                <ul className='flex gap-3 text-xs'>
-                  {value.map((image: string) => (
-                    <li key={crypto.randomUUID()}>
-                      <Image src={image} alt='Image' width={50} height={50} />
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <Image src={value} alt='Image' width={50} height={50} />
-              )}
-            </>
+          {defaultValue ? (
+            <ul className='flex gap-3 text-xs'>
+              {selectedFiles.map((url: any) => (
+                <li key={crypto.randomUUID()} className='relative'>
+                  <Image 
+                    src={typeof url === 'string' ? url : URL.createObjectURL(url)} 
+                    alt='Image' 
+                    width={50} 
+                    height={50} 
+                  />
+                  <button 
+                    type='button' 
+                    onClick={() => handleImageDelete(url)}
+                    className='absolute top-0 right-0 w-5 h-5 flex justify-center items-center text-xs bg-white'
+                  >
+                    <CloseOutlined />
+                  </button>
+                </li>
+              ))}
+            </ul>
           ) : (
-            <>
-              {defaultValue ? (
-                <ul className='flex gap-3 text-xs'>
-                  {selectedFiles.map((url: any) => (
-                    <li key={crypto.randomUUID()} className='relative'>
-                      <Image 
-                        src={typeof url === 'string' ? url : URL.createObjectURL(url)} 
-                        alt='Image' 
-                        width={50} 
-                        height={50} 
-                      />
-                      <button 
-                        type='button' 
-                        onClick={() => handleImageDelete(url)}
-                        className='absolute top-0 right-0 w-5 h-5 flex justify-center items-center text-xs bg-white'
-                      >
-                        <CloseOutlined />
-                      </button>
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <ul className='flex gap-3 text-xs'>
-                  {selectedFiles.map((file: any) => (
-                    <li key={crypto.randomUUID()} className='relative'>
-                      {/* <Image 
-                        src={URL.createObjectURL(file)} 
-                        alt='Image' 
-                        width={50} 
-                        height={50} 
-                      /> */}
-                      <span></span>
-                      <button 
-                        type='button' 
-                        onClick={() => handleImageDelete(file.name)}
-                        className='absolute top-0 right-0 w-5 h-5 flex justify-center items-center text-xs bg-white'
-                      >
-                        <CloseOutlined />
-                      </button>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </>
-            )}
+            <ul className='flex gap-3 text-xs'>
+              {selectedFiles.map((file: any) => (
+                <li key={crypto.randomUUID()} className='relative'>
+                  <Image 
+                    src={file} 
+                    alt='Image' 
+                    width={50} 
+                    height={50} 
+                  />
+                  <button 
+                    type='button' 
+                    onClick={() => handleImageDelete(file.name)}
+                    className='absolute top-0 right-0 w-5 h-5 flex justify-center items-center text-xs bg-white'
+                  >
+                    <CloseOutlined />
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
         <input 
           ref={ref}
           className={`hidden h-10 w-full text-sm `}
           id={name}
           type='file'
-          // name={name} 
           required={required} 
           multiple={multiple}
           onChange={handleChange}
