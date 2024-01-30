9c043bd929cb15831525718a8a9b50ccaba32c73
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-01-30 21:20:28 +0200

    fix styles

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -8,8 +8,10 @@ const Hero: React.FC = () => {
       <div className='absolute w-full h-full flex'>
         <div className='flex items-end hero-item'>
           <div className='pt-3 md:pt-6 pb-12 md:pb-0 bg-white'>
-            <h1 className='mb-3 md:mb-0 homepage-heading-primary text-center'>Decor House</h1>
-            <p className='py-6 hidden md:block'>
+            <h1 className='mb-3 md:mb-0 homepage-heading-primary text-center'>
+              Decor House
+            </h1>
+            <p className='my-6 hidden md:block'>
               We are design pioneers in the online world. Ever since we have grown continually into a medium-sized enterprise. The proud number of over 400,000 satisfied customers agrees with our pioneer work. 
             </p>
             <Link 
@@ -20,9 +22,9 @@ const Hero: React.FC = () => {
           </div>
         </div>
         <div className='hero-divider' />
-        <div className='hidden md:block hero-item'></div>
+        <div className='hidden md:block hero-item' />
         <div className='hidden md:block hero-divider' />
-        <div className='hero-item'></div>
+        <div className='hero-item' />
       </div>
     </div>
   );
