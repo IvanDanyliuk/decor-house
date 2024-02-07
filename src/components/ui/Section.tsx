acbb083fac9bd85e7bbece38fda2a455575c21d9
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-02-07 15:52:54 +0200

    code style

--- src/components/ui/Section.tsx
+++ src/components/ui/Section.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { motion, useAnimation, useInView } from 'framer-motion';
 import { ReactNode, useEffect, useRef } from 'react';
+import { motion, useAnimation, useInView } from 'framer-motion';
 
 
 interface ISection {
