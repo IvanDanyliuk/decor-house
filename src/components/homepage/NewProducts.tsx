bc7a9c5fee15fdc71eea6a63359da9d2083801dd
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-02-07 09:20:13 +0200

    add animations for the New Products component

--- src/components/homepage/NewProducts.tsx
+++ src/components/homepage/NewProducts.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { motion, AnimatePresence } from 'framer-motion';
 import { Divider } from 'antd';
 import { IProduct } from '@/lib/types/products.types';
 import SliderNavPanel from '../ui/SliderNavPanel';
@@ -52,23 +53,32 @@ const NewProducts: React.FC<INewProducts> = ({ products }) => {
             <Link 
               key={crypto.randomUUID()}
               href={`/catalog/${product._id}`} 
-              className='relative w-full md:w-1/3 bg-white'
+              className='w-full md:w-1/3'
             >
-              <div className='w-full min-h-max flex justify-center items-center'>
-                <Image 
-                  src={product.images[0]} 
-                  alt={product.name} 
-                  width={350} 
-                  height={350} 
-                />
-              </div>
-              <div className='mt-6 text-center text-lg font-bold'>
-                {product.name}
-              </div>
-              <Divider className='my-6' />
-              <div className='text-center text-2xl font-bold'>
-                &euro;{product.price}
-              </div>
+              <AnimatePresence>
+                <motion.div 
+                  className='relative w-full bg-white'
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                >
+                  <div className='w-full min-h-max flex justify-center items-center'>
+                    <Image 
+                      src={product.images[0]} 
+                      alt={product.name} 
+                      width={350} 
+                      height={350} 
+                    />
+                  </div>
+                  <div className='mt-6 text-center text-lg font-bold'>
+                    {product.name}
+                  </div>
+                  <Divider className='my-6' />
+                  <div className='text-center text-2xl font-bold'>
+                    &euro;{product.price}
+                  </div>
+                </motion.div>
+              </AnimatePresence>
             </Link>
           ))}
         </div>
