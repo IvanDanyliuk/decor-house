35ac540ca086399f4be84101dad76a717cfc8819
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-02-28 09:50:37 +0200

    add the empty-cart message for the Cart Details component

--- src/components/cart/CartDetails.tsx
+++ src/components/cart/CartDetails.tsx
@@ -72,47 +72,71 @@ const CartDetails: React.FC<ICartDetails> = ({ user }) => {
 
   return (
     <div className='py-0 md:py-8 container mx-auto'>
-      <ul className='w-full'>
-        {cartData.map((cartItem, i) => (
-          <div key={crypto.randomUUID()}>
-            <CartItem 
-              data={cartItem} 
-              onIncreaseQuantity={increaseProductQuantity} 
-              onDecreaseQuantity={decreaseProductQuantity} 
-              onDeleteItem={deleteProductFromCart} 
-            />
-            <Divider />
-          </div>
-        ))}
-      </ul>
-      {width && width < 640 && (
-        <p className='py-6 text-3xl text-right font-bold'>
-          Total: <span>&euro;{totalAmount}</span>
-        </p>
-      )}
-      <div className='py-6 flex justify-between items-center gap-3'>
-        <Link href='/catalog' className='cart-btn'>
-          {width && width >= 640 && (
-            <Image 
-              src='/assets/icons/left-arrow.svg'
-              alt='arrow-left'
-              width={30}
-              height={30}
-            />
-          )}
-          <span>Continue shopping</span>
-        </Link>
-        <div className='flex flex-1 md:flex-none items-center gap-8'>
-          {width && width >= 640 && (
-            <p className='text-xl font-bold'>
+      {cartData.length > 0 ? (
+        <>
+          <ul className='w-full'>
+            {cartData.map((cartItem, i) => (
+              <div key={crypto.randomUUID()}>
+                <CartItem 
+                  data={cartItem} 
+                  onIncreaseQuantity={increaseProductQuantity} 
+                  onDecreaseQuantity={decreaseProductQuantity} 
+                  onDeleteItem={deleteProductFromCart} 
+                />
+                <Divider />
+              </div>
+            ))}
+          </ul>
+          {width && width < 640 && (
+            <p className='py-6 text-3xl text-right font-bold'>
               Total: <span>&euro;{totalAmount}</span>
             </p>
           )}
-          <Link href='/checkout' className='cart-btn bg-accent-dark text-white'>
-            Checkout
-          </Link>
+          <div className='py-6 flex justify-between items-center gap-3'>
+            <Link href='/catalog' className='cart-btn'>
+              {width && width >= 640 && (
+                <Image 
+                  src='/assets/icons/left-arrow.svg'
+                  alt='arrow-left'
+                  width={30}
+                  height={30}
+                />
+              )}
+              <span>Continue shopping</span>
+            </Link>
+            <div className='flex flex-1 md:flex-none items-center gap-8'>
+              {width && width >= 640 && (
+                <p className='text-xl font-bold'>
+                  Total: <span>&euro;{totalAmount}</span>
+                </p>
+              )}
+              <Link href='/checkout' className='cart-btn bg-accent-dark text-white'>
+                Checkout
+              </Link>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className='w-full flex justify-center items-center'>
+          <div className='flex flex-col items-center gap-6'>
+            <Image 
+              src='/assets/icons/cart.png'
+              alt='previous'
+              width={300}
+              height={300}
+            />
+            <p className='text-5xl text-center font-bold'>
+              Cart is empty
+            </p>
+            <p className='text-xl text-center text-gray-dark font-semibold'>
+              Choose products and return to checkout an order
+            </p>
+            <Link href='/catalog' className='py-3 w-full md:w-72 link-primary'>
+              Catalog
+            </Link>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
