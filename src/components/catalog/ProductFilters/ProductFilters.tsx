31a0284c4c31993fd14af5e424b934885edb0e64
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-03-12 21:59:01 +0200

    fix the sorting products feature

--- src/components/catalog/ProductFilters/ProductFilters.tsx
+++ src/components/catalog/ProductFilters/ProductFilters.tsx
@@ -4,7 +4,7 @@ import { Select } from 'antd';
 import FilterSelect from './FilterSelect';
 import PriceFilter from './PriceFilter';
 import { IFilterItem, IProductFiltersData } from '@/lib/types/products.types';
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 
@@ -19,6 +19,12 @@ const ProductFilters: React.FC<IProductFilters> = ({ filtersData, sortData }) =>
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
+  const sortValueInitialState = searchParams.get('order') && searchParams.get('sortIndicator') ? 
+    JSON.stringify({ order: searchParams.get('order'), sortIndicator: searchParams.get('sortIndicator') }) : 
+    sortData[0].value;
+  
+  const [sortValue, setSortValue] = useState<string>(sortValueInitialState);
+
   const createSortingQueryString = useCallback(
     (order: string, sortIndicator: string) => {
       const params = new URLSearchParams(searchParams.toString());
@@ -31,9 +37,16 @@ const ProductFilters: React.FC<IProductFilters> = ({ filtersData, sortData }) =>
   );
 
   const handleSortChange = (value: string) => {
+    setSortValue(value);
     const parsedValue = JSON.parse(value);
     router.push(`${pathname}?${createSortingQueryString(parsedValue.order, parsedValue.sortIndicator)}`);
   };
+
+  useEffect(() => {
+    if(!searchParams.get('order') && !searchParams.get('sortIndicator')) {
+      setSortValue(sortData[0].value);
+    }
+  }, [searchParams]);
 
   return (
     <div className='w-full flex justify-between items-center'>
@@ -61,7 +74,8 @@ const ProductFilters: React.FC<IProductFilters> = ({ filtersData, sortData }) =>
       </div>
       <Select 
         options={sortData}
-        defaultValue={sortData[0].value}
+        // defaultValue={sortData[0].value}
+        value={sortValue}
         onChange={handleSortChange}
         className='w-52'
       />
