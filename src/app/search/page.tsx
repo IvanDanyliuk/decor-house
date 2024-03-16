db7c9cf09bd56f9383bb1ec821fb815a3511e91e
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-03-16 20:16:04 +0200

    add the Selected Filters section

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,10 @@
+import ResetFiltersButton from '@/components/catalog/ProductFilters/ResetFiltersButton';
+import SelectedFilters from '@/components/catalog/ProductFilters/SelectedFilters';
 import SearchFilters from '@/components/catalog/search/SearchFilters';
 import { getSearchFilterData } from '@/lib/queries/product.queries';
 import { ICategory } from '@/lib/types/category.types';
 import { IManufacturer } from '@/lib/types/manufacturer.types';
+import { Divider } from 'antd';
 
 
 const Search = async ({ params, searchParams }: any) => {
@@ -18,18 +21,29 @@ const Search = async ({ params, searchParams }: any) => {
       .types.map((item: string) => ({ label: item, value: item })) : 
     [];
   const manufacturerOptions = filterData.data.manufacturers.map((item: IManufacturer) => ({ label: item.name, value: item._id! }));
-  
-  console.log('SEARCH PAGE', typesOptions)
 
   return (
-    <div className='container mx-auto py-3'>
-      <SearchFilters 
-        categories={categoryOptions} 
-        types={typesOptions} 
-        manufacturers={manufacturerOptions} 
-        minPrice={filterData.data.minPrice} 
-        maxPrice={filterData.data.maxPrice} 
-      />
+    <div className='w-full'>
+      <section className='w-full py-6 bg-main-bg'>
+        <h2 className='container mx-auto page-heading-primary text-center'>
+          Search
+        </h2>
+      </section>
+      <section className='container mx-auto py-3'>
+        <SearchFilters 
+          categories={categoryOptions} 
+          types={typesOptions} 
+          manufacturers={manufacturerOptions} 
+          minPrice={filterData.data.minPrice} 
+          maxPrice={filterData.data.maxPrice} 
+        />
+        <Divider className='my-3' />
+        <div className='flex justify-between items-center'>
+          <SelectedFilters manufacturers={manufacturerOptions} />
+          <ResetFiltersButton />
+        </div>
+      </section>
+      
     </div>
   );
 };
--- src/components/catalog/search/SearchFilters.tsx
+++ src/components/catalog/search/SearchFilters.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { Select } from 'antd';
 import { productsSortItems } from '@/lib/constants';
