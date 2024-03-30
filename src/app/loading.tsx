a5a829c8ebb92fa0b37eaa1d3c8d7f8dc5497bae
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-03-30 19:35:52 +0200

    implement the Loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { LoadingOutlined } from '@ant-design/icons';
+
 
 const Loading = () => {
   return (
-    <div>loading...</div>
+    <div className='w-full py-72 flex justify-center items-center'>
+      <LoadingOutlined className='text-xl' />
+    </div>
   );
 };
 
