4759ee988e3bb3905dde22b1b6b18220badace16
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-04-29 10:26:58 +0300

    fix

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -7,12 +7,11 @@ export default function Error({
   error: Error, 
   reset: () => void 
 }) {
-  console.log('ERROR DETAILED INFORMATION', error)
   return (
-    <div className='w-full h-96 flex justify-center items-center'>
+    <div className='w-full h-96 flex flex-col justify-center items-center gap-3'>
       <p className='text-center text-xl text-gray-700 font-bold'>Something went wrong...</p>
       <p className='text-center text-lg text-gray-600 font-semibold'>{error.message}</p>
-      <button onClick={reset}>Try again!</button>
+      <button onClick={reset} className='px-5 py-4 bg-accent-dark text-white uppercase rounded'>Try again!</button>
     </div>
   );
 };
