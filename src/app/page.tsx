6e77593a8a084356ebed56cb10c5947bd9e4ed72
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-04-29 13:40:08 +0300

    new products fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default async function Home() {
         <StoreOverview />
       </Section>
       <Section className='mx-auto px-3 md:px-0 container'>
-        <NewProducts products={productsData.data.products} />
+        {/* <NewProducts products={productsData.data.products} /> */}
       </Section>
       <Section className='w-full'>
         <Demo />
