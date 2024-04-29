49a1d4b728258a15240ffc974a879f37de253b5a
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-04-29 10:05:33 +0300

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,20 +4,20 @@ import { NextResponse } from 'next/server';
 
 export default withAuth(
   function middleware(req) {
-    if(req.nextUrl.pathname.startsWith('/dashboard') && req.nextauth.token?.role !== 'admin') {
-      return NextResponse.rewrite(new URL('/login', req.url));
-    }
-    if(req.nextUrl.pathname.startsWith('/profile/*') && req.nextauth.token) {
-      return NextResponse.rewrite(new URL('/login', req.url))
-    }
+    // if(req.nextUrl.pathname.startsWith('/dashboard') && req.nextauth.token?.role !== 'admin') {
+    //   return NextResponse.rewrite(new URL('/login', req.url));
+    // }
+    // if(req.nextUrl.pathname.startsWith('/profile/*') && req.nextauth.token) {
+    //   return NextResponse.rewrite(new URL('/login', req.url))
+    // }
   },
   {
     callbacks: {
-      authorized: ({ token }) => !!token,
+      // authorized: ({ token }) => !!token,
     },
   },
 );
 
-export const config = { matcher: [
-  '/dashboard', '/dashboard/:path*', '/profile', '/profile/:path*', '/orders', '/orders/:path*'
-] };
+// export const config = { matcher: [
+//   '/dashboard', '/dashboard/:path*', '/profile', '/profile/:path*', '/orders', '/orders/:path*'
+// ] };
