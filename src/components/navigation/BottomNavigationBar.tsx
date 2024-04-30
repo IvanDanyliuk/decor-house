e89c756f049e714113dbfe188b961fe8e32d6151
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-04-30 19:14:38 +0300

    fix the BottomNavigationBar component

--- src/components/navigation/BottomNavigationBar.tsx
+++ src/components/navigation/BottomNavigationBar.tsx
@@ -5,11 +5,14 @@ import Link from 'next/link';
 import { Badge, Drawer } from 'antd';
 import { HomeOutlined, MoreOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import SearchMobile from '../layout/SearchMobile';
+import { useWindowSize } from '@/utils/hooks/use-window-size';
 
 
 const BottomNavigationBar: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [cartSize, setCartSize] = useState<number>(0);
+
+  const { width } = useWindowSize();
 
   const handleMenuOpen = () => {
     setIsOpen(prev => !prev);
@@ -34,11 +37,15 @@ const BottomNavigationBar: React.FC = () => {
     return () => window.removeEventListener('storage', handleStorageChange);
   }, []);
 
+  if(width && width > 640) {
+    return null;
+  }
+
   return (
     <>
       <button 
         onClick={handleMenuOpen} 
-        className='fixed right-3 bottom-3 w-14 h-14 flex justify-center items-center bg-accent-dark text-2xl text-white rounded-full shadow-lg'
+        className='fixed right-5 bottom-5 w-14 h-14 flex justify-center items-center bg-accent-dark text-2xl text-white rounded-full shadow-lg'
       >
         <MoreOutlined />
       </button>
