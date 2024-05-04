5bc6a7334a3ac7c41958b835e844011767a93571
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-05-04 12:41:11 +0300

    remove heading links in the Cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import type { Metadata } from 'next';
 import { getServerSession } from 'next-auth';
 import CartDetails from '@/components/cart/CartDetails';
@@ -18,12 +17,6 @@ const Cart = async () => {
   return (
     <div className='w-full'>
       <section className='px-3 md:px-0 py-8 w-full bg-main-bg'>
-        <p className='container mx-auto mb-4 flex gap-1 text-sm font-semibold capitalize'>
-          <Link href='/'>Home</Link>
-          <span>/</span>
-          <Link href='/catalog'>Catalog</Link>
-          <span>/</span>
-        </p>
         <h2 className='container mx-auto page-heading-primary'>Cart</h2>
       </section>
       <section className='container mx-auto px-3 md:px-0'>
